cefb99730e496c59544ca99816be0c5848b48c12
Joao Quiziri <quizirironchi@hotmail.com>
2024-07-22 18:57:24 -0300

    Add SQLite

---
+++ Controllers/ToDoQController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoQ.Contexts;
+using ToDoQ.Models;
+
+namespace ToDoQ.Controllers;
+
+public class ToDoQController : Controller
+{
+    private readonly ToDoQContext _context;
+
+    public ToDoQController(ToDoQContext context) {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var toDos = _context.ToDos.ToList();
+        return View(toDos);
+    }
+}
